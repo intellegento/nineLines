3d0da3b7343afa4a116be8931563866aa91fe41d
Gennadii Shpreer <intellegento@gmail.com>
2023-03-27 16:38:25 +0600

    fix: locomotive on mpbile devices

--- src/js/components/backToTop.js
+++ src/js/components/backToTop.js
@@ -1,36 +1,33 @@
 const init = () => {
-	const block = document.querySelectorAll('.block');
-window.addEventListener('load', function(){
-  block.forEach(item => {
-    let numElement = item.querySelector('.num');
-    let num = parseInt(100);
-    let count = 0;
-    let time = 2000 / num;
-    let circle = item.querySelector('.circle');
-    setInterval(() => {
-      if(count == num){
-        clearInterval();
-      } else {
-        count += 1;
-        numElement.innerText = count;
-      }
-    }, time)
-    circle.style.strokeDashoffset 
-      = 189 - ( 189 * ( num / 100 ));
-    let dots = item.querySelector('.dots');
-    dots.style.transform = 
-      `rotate(${360 * (num / 100)}deg)`;
-    if(num == 100){
-      dots.style.opacity = 0;
-    }
-  })
-});
-      
+	const block = document.querySelectorAll(".block");
+	window.addEventListener("load", function () {
+		block.forEach((item) => {
+			let numElement = item.querySelector(".num");
+			let num = parseInt(100);
+			let count = 0;
+			let time = 2000 / num;
+			let circle = item.querySelector(".circle");
+			setInterval(() => {
+				if (count == num) {
+					clearInterval();
+				} else {
+					count += 1;
+					numElement.innerText = count;
+				}
+			}, time);
+			circle.style.strokeDashoffset = 189 - 189 * (num / 100);
+			let dots = item.querySelector(".dots");
+			dots.style.transform = `rotate(${360 * (num / 100)}deg)`;
+			if (num == 100) {
+				dots.style.opacity = 0;
+			}
+		});
+	});
 };
 
 const destroy = () => {
-	helpers.$window.off('.backTop');
-	helpers.$document.off('.backTop');
+	helpers.$window.off(".backTop");
+	helpers.$document.off(".backTop");
 };
 
 export default {
